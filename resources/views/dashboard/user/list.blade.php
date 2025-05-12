9c8b99e22b18a4eb8831fd8fcf09d942aece247d
zpunk <senoardi01@gmail.com>
2025-05-12 14:30:47 +0700

    Styling halaman list user

--- resources/views/dashboard/user/list.blade.php
+++ resources/views/dashboard/user/list.blade.php
@@ -4,14 +4,14 @@
     <div class="card">
         <div class="card-header">
             <div class="row">
-                <div class="col-8">
+                <div class="col-8 align-self-center">
                     <h3>Users</h3>
                 </div>
 
                 <div class="col-4">
                     <form action="{{ url('dashboard/users') }}" method="get">
                         <div class="input-group">
-                            <input type="text" class="form-control" name="q" value="{{ $request['q'] ?? '' }}">
+                            <input type="text" class="form-control form-control-sm" name="q" value="{{ $request['q'] ?? '' }}">
                             <button type="submit" class="btn btn-secondary btn-sm ">Search</button>
                         </div>
                     </form>
@@ -34,7 +34,7 @@
                 <tbody>
                      @foreach ($users as $user)
                         <tr>
-                            <td>{{ ($users->currentPage() -1) * $users->perPage() + $loop->iteration}}</td>
+                            <th scope="row">{{ ($users->currentPage() -1) * $users->perPage() + $loop->iteration}}</th>
                             <td>{{$user->name}}</td>
                             <td>{{$user->email}}</td>
                             <td>{{$user->created_at}}</td>
