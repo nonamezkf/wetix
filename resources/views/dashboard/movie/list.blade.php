672c11958a63fcabcb4a319b526e9b25b42ccff6
zpunk <senoardi01@gmail.com>
2025-06-01 13:32:01 +0700

    Edit tampilan list movies jika data kosong

--- resources/views/dashboard/movie/list.blade.php
+++ resources/views/dashboard/movie/list.blade.php
@@ -19,42 +19,46 @@
             </div>
         </div>
         <div class="card-body p-0">
-            <table class="table table-striped table-hover table-borderless">
-                <thead>
-                    <tr>
-                        <th>No</th>
-                        <th>Title</th>
-                        <th>Thumbnail</th>
-                        <th>Description</th>
-                        <th>Edited</th>
-                        <th>&nbsp;</th>
-                    </tr>
-                </thead>
-                
-                <tbody>
-                     @foreach ($movies as $movie)
+            @if($movies->total())
+                <table class="table table-striped table-hover table-borderless">
+                    <thead>
                         <tr>
-                            <th scope="row">{{ ($movies->currentPage() -1) * $movies->perPage() + $loop->iteration}}</th>
-                            <td>{{$movie->name}}</td>
-                            <td>{{$movie->email}}</td>
-                            <td>{{$movie->created_at}}</td>
-                            <td>{{$movie->updated_at}}</td>
-                            <td>
-                                <!-- dibawah ini untuk contoh untuk routing menggunakan url() -->
-                                <!-- <a href="{{ url('dashboard/user/edit/'.$user->id) }}" class="btn btn-success btn-sm" title="edit" ><i class="fas fa-pen"></i></a> -->
-
-                                
-                                <!-- dibawah ini untuk routing menggunakan route() -->
-                                <a href="{{ route('dashboard.movie.edit', ['id' => $user->id]) }}" class="btn btn-success btn-sm" title="edit" ><i class="fas fa-pen"></i></a>
-                            </td>
+                            <th>No</th>
+                            <th>Title</th>
+                            <th>Thumbnail</th>
+                            <th>Description</th>
+                            <th>Edited</th>
+                            <th>&nbsp;</th>
                         </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                    </thead>
+                    
+                    <tbody>
+                        @foreach ($movies as $movie)
+                            <tr>
+                                <th scope="row">{{ ($movies->currentPage() -1) * $movies->perPage() + $loop->iteration}}</th>
+                                <td>{{$movie->name}}</td>
+                                <td>{{$movie->email}}</td>
+                                <td>{{$movie->created_at}}</td>
+                                <td>{{$movie->updated_at}}</td>
+                                <td>
+                                    <!-- dibawah ini untuk contoh untuk routing menggunakan url() -->
+                                    <!-- <a href="{{ url('dashboard/user/edit/'.$user->id) }}" class="btn btn-success btn-sm" title="edit" ><i class="fas fa-pen"></i></a> -->
 
-            {{$movies->appends($request)->links()}}
-            <!-- kode di atas berfungsi untuk menampilkan pagination  -->
-            <!-- dan jika variable request memiliki nilai maka akan di tambahkan setelah link pagination -->
+                                    
+                                    <!-- dibawah ini untuk routing menggunakan route() -->
+                                    <a href="{{ route('dashboard.movie.edit', ['id' => $user->id]) }}" class="btn btn-success btn-sm" title="edit" ><i class="fas fa-pen"></i></a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+
+                {{$movies->appends($request)->links()}}
+                <!-- kode di atas berfungsi untuk menampilkan pagination  -->
+                <!-- dan jika variable request memiliki nilai maka akan di tambahkan setelah link pagination -->
+            @else
+                <h4 class="text-center p-3">Data movie tidak tersedia</h4>
+            @endif
         </div>
     </div>
 
