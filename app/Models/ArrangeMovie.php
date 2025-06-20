4464d16ab99d8512e566ff25e2f51dd6b77944f9
zpunk <senoardi01@gmail.com>
2025-06-20 14:48:18 +0700

    Membuat relasi antar tabel movie dan theater

--- app/Models/ArrangeMovie.php
+++ app/Models/ArrangeMovie.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class ArrangeMovie extends Model
 {
     protected $table = 'arrange_movie';
+
+    public function movies(){
+        return $this->hasMany('App\Models\Movie', 'id', 'movie_id');
+    }
 }
