fd66230043641069213727dca99d72d5b5f944b4
zpunk <senoardi01@gmail.com>
2025-07-06 08:30:23 +0700

    Menambahkan pesan ketika arrange movie di hapus

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -5,6 +5,7 @@ return [
     'store' => 'Data :title berhasil disimpan',
     'update' => 'Update Data pada :title berhasil',
     'delete' => 'Penghapusan data :title berhasil',
+    'deteleArrangeMovie' => 'Data berhasil dihapus', 
     'no_data' => 'Data :module tidak tersedia'
 
 ];
