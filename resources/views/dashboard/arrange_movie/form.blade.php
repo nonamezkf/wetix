62908387a0387e263846c483cb8cdebc71283c92
zpunk <senoardi01@gmail.com>
2025-07-06 08:28:21 +0700

    Update form create + edit arrage movie

--- resources/views/dashboard/arrange_movie/form.blade.php
+++ resources/views/dashboard/arrange_movie/form.blade.php
@@ -18,15 +18,15 @@
         <div class="card-body">
             <div class="row">
                 <div class="col-md-8 offset-md-2">
-                    <form method="post" action="{{route($url, $theater->id ?? '' )}}" enctype="multipart/form-data">
-                        @csrf
-                        @if(isset($theater))
-                        <!-- mencoba menggunakan custom method bawaan laravel untuk update data -->
-                            {{-- @method('put') --}}
-                        @endif
+                    @if(isset($arrangeMovie))
+                        <form method="post" action="{{route($url, [$theater->id, $arrangeMovie->id] ?? '' )}}" enctype="multipart/form-data">
+                    @else
+                        <form method="post" action="{{route($url, $theater->id ?? '' )}}" enctype="multipart/form-data">
+                    @endif
+                        @csrf            
 
                         <div>
-                            <input type="hidden" name="theater_id" value="{{$theater->id}}">
+                            <input type="text" name="theater_id" value="{{$theater->id}}" hidden>
                         </div>
 
                         <div class="form-group">
@@ -34,7 +34,7 @@
                             <select name="movie_id" id="" class="form-control">
                                 <option value="">Pilih Movie</option>
                                 @foreach($movies AS $movie)
-                                    @if($movie->id == old('movie_id'))
+                                    @if($movie->id == (old('movie_id') ?? $arrangeMovie->movie_id ?? ''))
                                         <option value="{{$movie->id}}" selected>{{$movie->title}}</option>
                                     @else
                                         <option value="{{$movie->id}}">{{$movie->title}}</option>
@@ -48,7 +48,7 @@
 
                         <div class="form-group">
                             <label for="theater">Studio</label>
-                            <input type="text" class="form-control @error('studio') {{'is-invalid'}} @enderror" name="studio" value="{{ old('studio') ?? $theater->studio ?? '' }}">
+                            <input type="text" class="form-control @error('studio') {{'is-invalid'}} @enderror" name="studio" value="{{ old('studio') ?? $arrangeMovie->studio ?? '' }}">
                             <!-- kode dibawah ini untuk menampilkan pesan error dari validator -->
                             @error('studio')
                                 <span class="text-danger">{{$message}}</span>
@@ -57,7 +57,7 @@
 
                         <div class="form-group">
                             <label for="theater">Price</label>
-                            <input type="number" class="form-control @error('price') {{'is-invalid'}} @enderror" name="price" value="{{ old('price') ?? $theater->price ?? '' }}">
+                            <input type="number" class="form-control @error('price') {{'is-invalid'}} @enderror" name="price" value="{{ old('price') ?? $arrangeMovie->price ?? '' }}">
                             <!-- kode dibawah ini untuk menampilkan pesan error dari validator -->
                             @error('price')
                                 <span class="text-danger">{{$message}}</span>
@@ -65,17 +65,22 @@
                         </div>
 
                         <div class="form-group form-row mt-4">
+
+                            @php
+                                $seat = isset($arrangeMovie->seats) ? json_decode($arrangeMovie->seats) : false;     
+                            @endphp
+
                             <div class="col-2">
                                 <label for="seats">Seats</label>
                             </div>
                             <div class="col-5">
-                                <input type="number" class="form-control @error('rows') {{'is-invalid'}} @enderror" name="rows" value="{{ old('rows') ?? $theater->rows ?? '' }}" placeholder="rows">
+                                <input type="number" class="form-control @error('rows') {{'is-invalid'}} @enderror" name="rows" value="{{ old('rows') ?? $seat->rows ?? '' }}" placeholder="rows">
                                 @error('rows')
                                     <span class="text-danger">{{$message}}</span>
                                  @enderror
                             </div>
                             <div class="col-5">
-                                <input type="number" class="form-control @error('columns') {{'is-invalid'}} @enderror" name="columns" value="{{ old('columns') ?? $theater->rows ?? '' }}" placeholder="Columns">
+                                <input type="number" class="form-control @error('columns') {{'is-invalid'}} @enderror" name="columns" value="{{ old('columns') ?? $seat->columns ?? '' }}" placeholder="Columns">
                                 @error('columns')
                                     <span class="text-danger">{{$message}}</span>
                                 @enderror
@@ -87,7 +92,7 @@
                                 <div class="form-group mb-0">
                                     <label for="schedules">Schedules</label>
                                 </div>
-                                <schedule-component :old-schedules="{{ json_encode(old('schedules') ?? [] ) }}"></schedule-component>
+                                <schedule-component :old-schedules="{{ $arrangeMovie->schedules ?? json_encode(old('schedules') ?? []) }}"></schedule-component>
                             </div>
                             @error('schedules')
                                 <span class="text-danger ml-2">{{$message}}</span>
@@ -99,15 +104,15 @@
                                 <label for="status" class="">Status</label>
                             </div>
                             <div class="form-check form-check-inline">
-                                <input type="radio" name="status" class="form-check-input" value="coming soon" id="coming soon" @if((old('status') ?? $theater->status ?? '') == 'coming soon') checked @endif>
+                                <input type="radio" name="status" class="form-check-input" value="coming soon" id="coming soon" @if((old('status') ?? $arrangeMovie->status ?? '') == 'coming soon') checked @endif>
                                 <label for="coming soon" class="form-check-label">Coming soon</label>
                             </div>
                             <div class="form-check form-check-inline">
-                                <input type="radio" name="status" class="form-check-input" value="in theater" id="in theater" @if((old('status') ?? $theater->status ?? '') == 'in active') checked @endif>
+                                <input type="radio" name="status" class="form-check-input" value="in theater" id="in theater" @if((old('status') ?? $arrangeMovie->status ?? '') == 'in theater') checked @endif>
                                 <label for="in theater" class="form-check-label">In theater</label>
                             </div>
                             <div class="form-check form-check-inline">
-                                <input type="radio" name="status" class="form-check-input" value="finish" id="finish" @if((old('status') ?? $theater->status ?? '') == 'finish') checked @endif>
+                                <input type="radio" name="status" class="form-check-input" value="finish" id="finish" @if((old('status') ?? $arrangeMovie->status ?? '') == 'finish') checked @endif>
                                 <label for="finish" class="form-check-label">Finish</label>
                             </div>
                             <div>
@@ -128,7 +133,7 @@
         </div>
     </div>
 
-    @if(isset($theater))
+    @if(isset($arrangeMovie))
         <div class="modal fade" id="deleteModal">
             <div class="modal-dialog">
                 <div class="modal-content">
@@ -138,10 +143,10 @@
                         <button class="close" type="button" data-dismiss="modal">x</button>
                     </div>
                     <div class="modal-body">
-                        <p>Anda yakin ingin menghapus data theater {{$theater->title}}?</p>
+                        <p>Anda yakin ingin menghapus data ini ?</p>
                     </div>
                     <div class="modal-footer">
-                        <form action="{{ route('dashboard.theaters.delete', $theater->id) }}" method="post">
+                        <form action="{{ route('dashboard.arrange.movie.delete', [$theater->id, $arrangeMovie->id]) }}" method="post">
                             @csrf
                             <!-- mencoba menggunakan custom method bawaan laravel untuk delete data -->
                             @method("delete")
